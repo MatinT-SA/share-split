7c4b85b02d845abb5427eb8c0c96f19b5bbb13e1
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2024-11-27 21:17:41 +0330

    split bill form logic

--- src/App.js
+++ src/App.js
@@ -176,6 +176,11 @@ function FormAddFriend({ onAddFriend }) {
 }
 
 function FormSplitBill({ selectedFriend }) {
+  const [bill, setBill] = useState("");
+  const [myExpense, setMyExpense] = useState("");
+  const friendExpense = bill ? bill - myExpense : "";
+  const [turn, setTurn] = useState("You");
+
   return (
     <form className="form-split-bill">
       <h2>Split a bill with {selectedFriend.name}</h2>
@@ -184,28 +189,42 @@ function FormSplitBill({ selectedFriend }) {
         <AiOutlineFileText className="icons" />
         Bill
       </label>
-      <input type="text" />
+      <input
+        type="text"
+        value={bill}
+        onChange={(e) => setBill(Number(e.target.value))}
+      />
 
       <label>
         <AiOutlineUser className="icons" />
         Your Expense
       </label>
-      <input type="text" />
+      <input
+        type="text"
+        value={myExpense}
+        onChange={(e) =>
+          setMyExpense(
+            Number(e.target.value) > bill ? myExpense : Number(e.target.value)
+          )
+        }
+      />
 
       <label>
         <AiOutlineTeam className="icons" />
         {selectedFriend.name}'s Expense
       </label>
-      <input type="text" disabled />
+      <input type="text" disabled value={friendExpense} />
 
       <label>
         <FaHandHoldingUsd className="icons" />
         Who's Paying?
       </label>
-      <select>
+      <select value={turn} onChange={(e) => setTurn(e.target.value)}>
         <option value="user">You</option>
         <option value="friend">{selectedFriend.name}</option>
       </select>
+
+      <Button>Split Bill</Button>
     </form>
   );
 }

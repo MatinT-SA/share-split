162c50a593e78042e3b062faf546c937424850ac
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2024-11-29 23:12:10 +0330

    fixing logo github

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,7 +3,11 @@ import React from "react";
 const Logo = () => {
   return (
     <div>
-      <img className="logo" src="/images/logo.png" alt="Share Split Logo" />
+      <img
+        className="logo"
+        src={`${process.env.PUBLIC_URL}/images/logo.png`}
+        alt="Share Split Logo"
+      />
     </div>
   );
 };

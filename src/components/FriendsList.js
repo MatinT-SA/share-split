8d267a1171233ac8844337aac5d29d9434ec681c
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2024-11-29 14:02:36 +0330

    implementing pagination logic for friends list

--- src/components/FriendsList.js
+++ src/components/FriendsList.js
@@ -1,16 +1,46 @@
+import { useState } from "react";
 import { Friend } from "./Friend";
 
 export function FriendsList({ friends, onSelectFriend, selectedFriend }) {
+  const [currentPage, setCurrentPage] = useState(1);
+  const friendsPerPage = 6;
+
+  const totalPages = Math.ceil(friends.length / friendsPerPage);
+  const startIndex = (currentPage - 1) * friendsPerPage;
+  const endIndex = startIndex + friendsPerPage;
+  const currentFriends = friends.slice(startIndex, endIndex);
+
+  function handleNextPage() {
+    setCurrentPage((page) => Math.min(page + 1, totalPages));
+  }
+
+  function handlePreviousPage() {
+    setCurrentPage((page) => Math.max(page - 1, 1));
+  }
+
   return (
-    <ul>
-      {friends.map((friend) => (
-        <Friend
-          friend={friend}
-          key={friend.id}
-          onSelectFriend={onSelectFriend}
-          selectedFriend={selectedFriend}
-        />
-      ))}
-    </ul>
+    <div>
+      <ul>
+        {currentFriends.map((friend) => (
+          <Friend
+            friend={friend}
+            key={friend.id}
+            onSelectFriend={onSelectFriend}
+            selectedFriend={selectedFriend}
+          />
+        ))}
+      </ul>
+      <div className="pagination">
+        <button onClick={handlePreviousPage} disabled={currentPage === 1}>
+          Previous
+        </button>
+        <span>
+          Page {currentPage} of {totalPages}
+        </span>
+        <button onClick={handleNextPage} disabled={currentPage === totalPages}>
+          Next
+        </button>
+      </div>
+    </div>
   );
 }
